f0d56e874a934416e7c7e0d572774c9cec1e7be0
Omar Ali <x20mar@gmail.com>
2013-07-12 04:39:41 +0100

    Modifed grader.js

--- grader.js
+++ grader.js
@@ -1,31 +1,28 @@
 #!/usr/bin/env node
 /*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
+ * Automatically grade files for the presence of specified HTML tags/attributes.
+ * Uses commander.js and cheerio. Teaches command line application development
+ * and basic DOM parsing.
+ * 
+ * References: + cheerio - https://github.com/MatthewMueller/cheerio -
+ * http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/ -
+ * http://maxogden.com/scraping-with-node.html + commander.js -
+ * https://github.com/visionmedia/commander.js -
+ * http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy +
+ * JSON - http://en.wikipedia.org/wiki/JSON -
+ * https://developer.mozilla.org/en-US/docs/JSON -
+ * https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+ */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://obscure-island-7573.herokuapp.com/";
+var urlHTML = null;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +31,18 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertURLExists = function(inurl) {
+    var instr = inurl.toString();
+    rest.get(instr).on('complete', function(result) {
+    	  if (result instanceof Error) {
+    		  console.log('%s Error: ' + result.message);
+    		  process.exit(1);
+    	  } else {
+    	    urlHtml = result;
+    	  }
+    	});
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -55,6 +64,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkString = function(checksfile) {
+	$ = cheerio.load(urlHTML);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,8 +85,16 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_path>', 'URL to check', clone(assertURLExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    
+    var checkJson = new Object();
+    if(program.url !== ""){
+    	checkJson = checkString(program.checks);
+    }else{
+    	checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
